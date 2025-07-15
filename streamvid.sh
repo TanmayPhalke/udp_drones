3f6da7a200b19f83092cb2cb49a76bda5b8009c4
TanmayPhalke <31072498+TanmayPhalke@users.noreply.github.com>
2025-07-15 13:01:55 +0530

    Create streamvid.sh

---
+++ streamvid.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Usage: ./streamvid.sh <GCS_IP> <DRONE_ID>
+
+GCS_IP=$1
+DRONE_ID=$2
+PORT=$((8000 + DRONE_ID))
+
+ffmpeg \
+  -f v4l2 -input_format mjpeg -video_size 640x480 -framerate 10 -i /dev/video0 \
+  -c:v libx264 -preset ultrafast -tune zerolatency -pix_fmt yuv420p \
+  -f mpegts "udp://${GCS_IP}:${PORT}?pkt_size=1316"
